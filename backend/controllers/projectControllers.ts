3b282921c112e0dd9c65c5d4dc5420dd33641926
Sab <sabinorotondi2000@gmail.com>
2022-11-01 13:09:05 +0100

    feat: added http methods to backend

--- backend/controllers/projectControllers.ts
+++ backend/controllers/projectControllers.ts
@@ -1,29 +1,73 @@
+const mongoose = require("mongoose");
 import { Response, Request } from "express";
+const asyncHandler = require("express-async-handler");
+const Project = require("../models/projectModel");
 
-const getProjects = (req: Request, res: Response) => {
-  res.status(200).json({ message: "Get All Projects" });
-};
+const getProjects = asyncHandler(async (req: Request, res: Response) => {
+  const projects = await Project.find();
+  res.status(200).json(projects);
+});
 
-const createProject = (req: Request, res: Response) => {
+const createProject = asyncHandler(async (req: Request, res: Response) => {
   if (!req.body.title) {
     res.status(400);
     // .json({ message: "Title is required" });
     throw new Error("Title is required"); //another way
   }
-  res.status(200).json({ message: "Create a Project" });
-};
+  const project = await Project.create(req.body);
+  if (!project) {
+    res.status(400);
+    throw new Error("Project not created");
+  }
+  res.status(201).json(project);
+});
 
-const getProject = (req: Request, res: Response) => {
-  res.json({ message: `Get Project ${req.params.id}` });
-};
+const getProject = asyncHandler(async (req: Request, res: Response) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400);
+    throw new Error(`${req.params.id} is not a valid id`);
+  }
+  const project = await Project.findById(req.params.id);
+  if (!project) {
+    res.status(404);
+    throw new Error("Project not found");
+  }
+  res.status(200).json(project);
+});
 
-const updateProject = (req: Request, res: Response) => {
-  res.json({ message: `Delete Project ${req.params.id}` });
-};
+const updateProject = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.body.title) {
+    res.status(400);
+    throw new Error("Title is required");
+  }
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400);
+    throw new Error(`${req.params.id} is not a valid id`);
+  }
 
-const deleteProject = (req: Request, res: Response) => {
-  res.json({ message: `Update Project ${req.params.id}` });
-};
+  const project = await Project.findByIdAndUpdate(req.params.id, req.body);
+  if (!project) {
+    res.status(404);
+    throw new Error("Project not found");
+  }
+  res.json(project);
+});
+
+const deleteProject = asyncHandler(async (req: Request, res: Response) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400);
+    throw new Error(`${req.params.id} is not a valid id`);
+  }
+  const project = await Project.findByIdAndDelete(req.params.id);
+  if (!project) {
+    res.status(404);
+    throw new Error("Project not found");
+  }
+  res.status(200).json({
+    message: `Project ${req.params.id} deleted`,
+    project: project,
+  });
+});
 
 module.exports = {
   getProjects,
